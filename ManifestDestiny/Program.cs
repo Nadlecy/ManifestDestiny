4930e497e9908112f765c3b225fab4c55447fd72
djorgeafonso <djorgeafonso@GCFRLY237>
2024-03-19 15:22:07 +0100

    s

--- ManifestDestiny/Program.cs
+++ ManifestDestiny/Program.cs
@@ -1,3 +1,16 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ManifestDestiny;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        WorldMap worldMap = new WorldMap();
+        worldMap.Init();
+        worldMap.CreateMap("Map01.txt");
+        Display display = new Display();
+        display.SetDiplay(worldMap.WorldMapTiles);
+        display.ShowDisplay();
+    }
+}
