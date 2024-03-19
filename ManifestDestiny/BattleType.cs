fabfa932d08a5b1b68a8399af345d3fd702b7a71
Nadlecy <bruno.cleymand@gmail.com>
2024-03-19 13:12:38 +0100

    dfsfdfsd

--- ManifestDestiny/BattleType.cs
+++ ManifestDestiny/BattleType.cs
@@ -6,40 +6,76 @@ using System.Threading.Tasks;
 
 class BattleType
 {
-    public string _name;
-    public Dictionary<string, float> _typeRelations = new Dictionary<string, float>();
-    public string _statusEffectImmunity;
+    string _name;
+    Dictionary<string, float> _typeRelations = new Dictionary<string, float>();
+    string _statusEffectImmunity;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public string StatusEffectImmunity
+    {
+        get => _statusEffectImmunity;
+        set => _statusEffectImmunity = value;
+    }
 
     public BattleType(string typeName)
     {
         _name = typeName;
 
         _typeRelations.Add("Scramble",1.0f);
+        _typeRelations.Add("Occult", 1.0f);
+        _typeRelations.Add("Mechanic", 1.0f);
+        _typeRelations.Add("Wild", 1.0f);
+        _typeRelations.Add("Fluid", 1.0f);
+        _typeRelations.Add("Vermin", 1.0f);
+        _typeRelations.Add("Absolute", 1.0f);
 
         switch (_name)
         {
             case "Scramble":
-
+                _typeRelations["Wild"] = 2.0f;
+                _typeRelations["Fluid"] = 0.5f;
                 break;
 
             case "Occult":
-
+                _typeRelations["Occult"] = 0.5f;
+                _typeRelations["Mechanic"] = 0.5f;
+                _typeRelations["Wild"] = 2.0f;
+                _typeRelations["Vermin"] = 2.0f;
                 break;
 
             case "Mechanic":
-
+                _typeRelations["Scramble"] = 0.5f;
+                _typeRelations["Occult"] = 2.0f;
+                _typeRelations["Mechanic"] = 0.5f;
+                _typeRelations["Wild"] = 0.5f;
+                _typeRelations["Fluid"] = 2.0f;
+                _typeRelations["Vermin"] = 0.0f;
                 break;
 
             case "Wild":
-
+                _typeRelations["Mechanic"] = 2.0f;
+                _typeRelations["Fluid"] = 0.5f;
+                _typeRelations["Vermin"] = 2.0f;
                 break;
 
             case "Fluid":
-
+                _typeRelations["Scramble"] = 0.0f;
+                _typeRelations["Occult"] = 2.0f;
+                _typeRelations["Mechanic"] = 0.5f;
+                _typeRelations["Wild"] = 0.5f;
                 break;
 
             case "Vermin":
-
+                _typeRelations["Scramble"] = 2.0f;
+                _typeRelations["Mechanic"] = 2.0f;
+                _typeRelations["Wild"] = 0.5f;
+                _typeRelations["Fluid"] = 2.0f;
+                _typeRelations["Vermin"] = 0.5f;
                 break;
 
             default:
