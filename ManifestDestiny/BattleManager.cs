f579f981429710b7c69a44d95c93761149b94678
Nadlecy <bruno.cleymand@gmail.com>
2024-03-19 15:28:29 +0100

    dsqdsqdsq

---
+++ ManifestDestiny/BattleManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BattleManager
+{
+
+    public BattleManager()
+    {
+
+    }
+
+    public void StartBattle()
+    {
+
+    }
+
+    public void PlayerChoice()
+    {
+
+    }
+
+    public bool Escape()
+    {
+
+        return true;
+    }
+
+    public void EndBattle()
+    {
+
+    }
+}
+
