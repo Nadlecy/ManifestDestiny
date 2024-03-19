05538d28d542f16e0e10e728ceeb9a1ff7fff171
Noham Coulon <44196664+coucoul38@users.noreply.github.com>
2024-03-19 14:06:23 +0100

    added a shitton of fields to seraph class :wolf:

--- ManifestDestiny/Seraph.cs
+++ ManifestDestiny/Seraph.cs
@@ -8,24 +8,69 @@ class Seraph
 {
     public string Name { get; set; }
     public string Type { get; set; }
-    public int Hp { get; set; }
-    public int Power { get; set; }
-    public int Resistance { get; set; }
-    public int Mana { get; set; }
-    public int Magic { get; set; }
-    public int Speed { get; set; }
     public string Description { get; set; }
 
-    public Seraph(string name, string type, int hp, int power, int resistance, int mana, int magic, int speed, string description)
+    public int Level { get; private set; }
+    public int _experience;
+    public int _experienceReward;
+
+    //enum StatusEffect
+    //{
+    //    Poison,
+    //    Stun,
+    //    Bleed,
+
+    //}
+
+    public enum Stats
+    {
+        hp,
+        power,
+        resistance,
+        mana,
+        magic,
+        speed
+    }
+
+    Dictionary<Stats, int> _baseStats;
+    Dictionary<Stats, int> _currentStats;
+    Dictionary<Stats, int> _statsAlterations;
+    //Dictionary<StatusEffect, int> Effect;
+
+    Dictionary<int, Ability> _abilitiesUnlocks; // List of all abilities this seraph can have, and the level at which it unlocks them.
+    Ability[] _abilities;
+    Dictionary<int, int> _xpForLevel;
+
+    public Seraph(string name, string type, Dictionary<Stats, int> baseStats, Dictionary<int,int> xpForLevel, Ability[] abilities, Dictionary<int, Ability> AbilitiesUnlocks, string description)
     {
         Name = name;
         Type = type;
-        Hp = hp;
-        Power = power;
-        Resistance = resistance;
-        Mana = mana;
-        Magic = magic;  
-        Speed = speed;
         Description = description;
+
+        _baseStats = baseStats;
+        _currentStats = baseStats;
+        _statsAlterations = new Dictionary<Stats, int>()
+        {
+            {Stats.power, 0 },
+            {Stats.resistance, 0 },
+            {Stats.mana, 0 },
+            {Stats.magic, 0 },
+            {Stats.speed, 0 },
+        };
+        
+        _abilities = abilities;
+        _abilitiesUnlocks = AbilitiesUnlocks;
+
+        Level = 0;
+        _experience = 0;
+        _xpForLevel = xpForLevel;
+    }
+
+    public int Experience {
+        get => _experience;
+        set { 
+            _experience = value;
+            // Compare to see if lvl up
+        }
     }
 }
