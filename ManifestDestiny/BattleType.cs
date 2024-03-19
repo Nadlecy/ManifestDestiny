b86560fbea2895f4c74c7c73768209171293d612
Nadlecy <bruno.cleymand@gmail.com>
2024-03-19 12:51:00 +0100

    init thingit

---
+++ ManifestDestiny/BattleType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BattleType
+{
+    public string _name;
+    public Dictionary<string, float> _typeRelations = new Dictionary<string, float>();
+    public string _statusEffectImmunity;
+
+    public BattleType(string typeName)
+    {
+        _name = typeName;
+
+        _typeRelations.Add("Scramble",1.0f);
+
+        switch (_name)
+        {
+            case "Scramble":
+
+                break;
+
+            case "Occult":
+
+                break;
+
+            case "Mechanic":
+
+                break;
+
+            case "Wild":
+
+                break;
+
+            case "Fluid":
+
+                break;
+
+            case "Vermin":
+
+                break;
+
+            default:
+                _name = "Absolute";
+                break;
+
+        }
+    }
+}
