7c9b874f8b3b5f14b38036d075c3b233ba262e5d
djorgeafonso <djorgeafonso@gaming.tech>
2024-03-25 14:03:50 +0100

    s

---
+++ ManifestDestiny/Seraph/AbilityContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManifestDestiny.Seraph
+{
+    internal class AbilityContainer
+    {
+        Dictionary<string, AbilityData> _ability;
+    }
+
+    internal class AbilityData
+    {
+        string _name;
+        string _type;
+        string _description;
+        int _power;
+        int _accuracy;
+        int _manaCost;
+        int _critChance;
+    }
+}
